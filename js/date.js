3bb93cdcbc755f7bda10321e2da8a734632e0e92
rdeluna801 <rdeluna801@byui.edu>
2022-01-13 20:28:43 -0700

    beautified pages

--- js/date.js
+++ js/date.js
@@ -1,17 +1,18 @@
 let daynames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednsday",
-    "Thursday",
-    "Friday",
-    "Saturday"];
-    
-  let months = [
-    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-  ];
-  let d = new Date();
-  let dayName = daynames[d.getDay()];
-  let monthName = months[d.getMonth()];
-  let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
-  document.getElementById("currentdate").textContent = fulldate;
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednsday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let months = [
+  "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+document.getElementById("currentdate").textContent = fulldate;
